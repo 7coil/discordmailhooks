fdbe9f66bb380ac625639eb9e6b496e441ed97d6
lepon01 <katie@discordmail.com>
2018-12-11 02:12:40 +0000

    Update bot counters

--- discord/botlist.js
+++ discord/botlist.js
@@ -3,49 +3,27 @@ const config = require('./../config/discord.json');
 
 module.exports = (client) => {
   if (config.api.botsdiscordpw) {
-    const botsdiscordpw = {
-      url: `https://bots.discord.pw/api/bots/${client.user.id}/stats`,
-      method: 'POST',
-      json: true,
+    fetch(`https://discord.bots.gg/api/bots/${client.user.id}/stats`, {
+      method: 'post',
+      body: JSON.stringify({
+        guildCount: client.guilds.size
+      }),
       headers: {
-        'User-Agent': config.useragent,
-        authorization: config.api.botsdiscordpw,
-      },
-      body: {
-        server_count: client.guilds.size,
-      },
-    };
-    request.post(botsdiscordpw);
-  }
-  if (config.api.discordbotsorg) {
-    const discordbotsorg = {
-      url: `https://discordbots.org/api/bots/${client.user.id}/stats`,
-      method: 'POST',
-      json: true,
-      headers: {
-        'User-Agent': config.useragent,
-        authorization: config.api.discordbotsorg,
-      },
-      body: {
-        server_count: client.guilds.size,
-      },
-    };
-    request.post(discordbotsorg);
-  }
-  if (config.api.discordfork && config.api.discordfork.guild && config.api.discordfork.channel && config.api.discordfork.for) {
-    const guild = client.guilds.get(config.api.discordfork.guild);
-    if (guild) {
-      const channel = guild.channels.get(config.api.discordfork.channel);
-      if (channel) {
-        channel.createMessage(JSON.stringify({
-          for: config.api.discordfork.for,
-          botCount: client.guilds.size
-        }, null, 2)).then(() => {
-          console.log('Posted to Discord Fork');
-        }).catch(() => {
-          console.log('Failed to post to Discord Fork');
-        });
+        'Content-Type': 'application/json'
       }
-    }
+    })
+  }
+  if (config.api.ls) {
+    fetch(`https://ls.terminal.ink/api/bots/${client.user.id}`, {
+      method: 'post',
+      body: JSON.stringify({
+        bot: {
+          count: client.guilds.size
+        }
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 };
